f9e50c11af9ec352c422b0865dde1d0833713b33
flect-endo <takumi.endo@flect.co.jp>
2015-02-25 10:07:51 +0900

    Fix indent

--- www/js/app.js
+++ www/js/app.js
@@ -47,15 +47,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     }
   })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent': {
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
+  .state('app.playlists', {
+    url: "/playlists",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/playlists.html",
+        controller: 'PlaylistsCtrl'
       }
-    })
+    }
+  })
 
   .state('app.single', {
     url: "/playlists/:playlistId",
