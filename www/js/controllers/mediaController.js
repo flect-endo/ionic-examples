eb203750393415c0a093e954bf75fd9a285a5800
flect-endo <takumi.endo@flect.co.jp>
2015-03-04 09:25:15 +0900

    Media.startRecord呼び出しコード追加

--- www/js/controllers/mediaController.js
+++ www/js/controllers/mediaController.js
@@ -1,4 +1,20 @@
 angular.module('starter.controllers')
 .controller('MediaCtrl', function($scope) {
   $scope.hoge = "hogehoge";
+
+  document.addEventListener("deviseready", onDeviseReady, false);
+
+  function onDeviseReady() {
+    alert("devise readied!");
+    var src = "hoge.mp3";
+    var media = new Media(src,
+      function() {
+        alert("success");
+      },
+      function(err) {
+        alert("error: " + err.code);
+      }
+    );
+    media.startRecord();
+  }
 });
